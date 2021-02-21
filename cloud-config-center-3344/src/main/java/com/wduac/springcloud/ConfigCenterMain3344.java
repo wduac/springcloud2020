7320f1bfde28ea551c68731794ccce681a8d4e49
wduac <wduac@connect.ust.hk>
2021-02-21 19:57:37 +0800

    add config-3344

---
+++ cloud-config-center-3344/src/main/java/com/wduac/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.wduac.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wduac
+ * @date 2021/2/21 - 17:47
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
